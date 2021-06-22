b5b394a56eeb996d971e73a6c34fff9ff6f70523
Juan Cuadrado <juandavidnew@gmail.com>
2021-06-22 12:14:52 -0500

    Test 5

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const twilio = require('twilio');
-const VoiceResponse = require('twilio').twiml.VoiceResponse;
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
 require('dotenv').config();
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = twilio(accountSid, authToken);
+
+const VoiceResponse = twilio.twiml.VoiceResponse;
+const MessagingResponse = twilio.twiml.MessagingResponse;
 
 const app = express();
 
@@ -13,7 +18,36 @@ app.get('/', (req, res) => {
         ok: true,
         mensaje: "Prueba"
     });
-})
+});
+
+app.post('/call', async (req, res) => {
+    try {
+        const twiml = new VoiceResponse();
+
+        // Use the <Gather> verb to collect user input
+        const gather = twiml.gather({ numDigits: 1 });
+        gather.say('For sales, press 1. For support, press 2.');
+
+        const call = await client.calls.create({
+            url: 'http://demo.twilio.com/docs/voice.xml',
+            to: '+573136314570',
+            from: '+19546268045',
+            twiml: twiml.toString()
+        });
+
+        const callSid = call.sid;
+
+        res.json({
+            ok: true,
+            resultado: callSid
+        });
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            resultado: error
+        });
+    }
+});
 
 app.post('/voice', twilio.webhook(), (req, res) => {
     const twiml = new VoiceResponse();
